68568a3d29d11d807dc532e2dddedde74f052e1b
MarcoErice <marco.erice@gmail.com>
2017-03-21 10:27:38 +0100

    Add project files.

---
+++ ResponsiveSharePointHostedAddIn/Pages/Default.aspx
@@ -0,0 +1,104 @@
+﻿<%-- The following 4 lines are ASP.NET directives needed when using SharePoint components --%>
+
+<%@ Page Inherits="Microsoft.SharePoint.WebPartPages.WebPartPage, Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" MasterPageFile="~masterurl/default.master" Language="C#" %>
+
+<%@ Register TagPrefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+
+<%-- The markup and script in the following Content element will be placed in the <head> of the page --%>
+<asp:Content ContentPlaceHolderID="PlaceHolderAdditionalPageHead" runat="server">
+    <!-- CSS styles added to the following file -->
+    <link type="text/css" href="../Content/App.css" rel="Stylesheet"/>
+    <link type="text/css" href="../Content/toastr.css" rel="stylesheet" />
+    <link type="text/css" href="../Content/bootstrap.css" rel="stylesheet" />
+    <link type="text/css" href="../Content/bootstrap-dialog.css" rel="stylesheet" />
+    <link type="text/css" href="../Content/DataTables/css/select.bootstrap.min.css" rel="stylesheet" />
+    <link type="text/css" rel="stylesheet" href="https://cdn.datatables.net/1.10.8/css/jquery.dataTables.css">
+     <!-- javascript references added to the following file -->
+    <script type="text/javascript" src="../Scripts/jquery-1.9.1.min.js"></script>
+    <script type="text/javascript" src="../Scripts/bootstrap.js"></script>
+    <script type="text/javascript" src="../Scripts/bootstrap-dialog.js"></script>
+    <script type="text/javascript" src="../Scripts/toastr.min.js"></script>
+    <script type="text/javascript" src="../Scripts/App.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.8/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="../Scripts/DataTables/dataTables.select.min.js"></script>
+</asp:Content>
+
+<%-- The markup and script in the following Content element will be placed in the <body> of the page --%>
+<asp:Content ContentPlaceHolderID="PlaceHolderMain" runat="server">
+    <div class="container">
+        <h1><span class="label label-primary">Favorite files</span></h1>
+        <div id="toolbar">
+            <button type="button" value="Files" class="btn btn-info" onclick="Javascript: location.href = '../Lists/Files'">
+                <span class='glyphicon glyphicon-upload'></span>
+                SP List
+            </button>
+            <button type="button" class="btn btn-success" onclick='addNewFile();'>
+                <span class='glyphicon glyphicon-plus'></span>
+                Add New Item
+            </button>
+        </div>
+   <p></p>
+ <div id="FilesPanel">
+ <table style="width: 100%;">
+ <tr>
+     <td>
+        <div id="FilesGrid" style="width: 100%"></div>
+     </td>
+ </tr>
+ </table>
+ </div>
+ <!-- Bootstrap Modal Dialog-->
+<div class="modal fade" id="myModalNorm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">
+                       <span aria-hidden="true">&times;</span>
+                       <span class="sr-only">Close</span>
+                </button>
+                <h4 class="modal-title" id="myModalLabel">
+                    Add New Item
+                </h4>
+            </div>
+            <!-- Modal Body -->
+            <div class="modal-body" id="modalBody">
+                <form role="form" id="fileForm">
+                  <div class="form-group">
+                    <label>Title</label>
+                      <input class="form-control" id="fileTitle"/>
+                  </div>
+                  <div class="form-group">
+                    <label>Name</label>
+                      <input class="form-control" id="fileName"/>
+                  </div>
+                  <div class="form-group">
+                    <label>File Type</label>
+                      <input class="form-control" id="fileType"/>
+                  </div>
+                     <div class="form-group">
+                    <label>Team</label>
+                      <input class="form-control" id="team"/>
+                  </div>
+                      <!-- hidden controls -->
+                  <div style="display: none">
+                      <input id="etag" />
+                      <input id="fileId" />
+                  </div>
+                  </form>
+                  <div class="modal-footer">
+                      <button type="button" class="btn btn-danger" data-dismiss="modal" onclick='updateFormLabel();'>
+                        Cancel
+                      </button>
+                      <button type="submit" class="btn btn-primary" id="fileFormSubmit">
+                        Submit
+                      </button>
+                  </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</asp:Content>
---
+++ ResponsiveSharePointHostedAddIn/Scripts/App.js
@@ -0,0 +1,215 @@
+﻿'use strict';
+var RestUrl = "../_api/lists/getbytitle('Files')/items";
+$(document).ready(function () {
+    PopulateGrid();
+    $('#fileFormSubmit').click(function (e) {
+        //Check for edit or new and call update or add function
+        if ($('#myModalLabel').html() == 'Add New Item') {
+            addFile($('#fileTitle').val(), $('#fileName').val(), $('#fileType').val(), $('#team').val());
+        } else {
+            UpdateFiles($('#fileId').val());
+        }
+    });
+});
+function PopulateGrid() {
+    //Clear datatables
+    $('#FilesGrid').empty();
+    //Get File list items
+    $.ajax({
+        url: RestUrl,
+        method: "GET",
+        headers: {
+            "accept": "application/json;odata=verbose"
+        },
+        success: function (data) {
+            if (data.d.results.length > 0) {
+                //construct HTML Table from the JSON Data
+                $('#FilesGrid').append(GenerateTableFromJson(data.d.results));
+                //Bind the HTML data with Jquery DataTable
+                var oTable = $('#FilesTable').dataTable({
+                    //control which datatable options available
+                    dom: 'Bfrltip',
+                    //add select functionality to datatable
+                    select: true,
+                    //adjust column widths
+                    "columns": [
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    { "width": "8%" }
+                    ],
+                    //remove sort icon from actions column
+                    "aoColumnDefs": [
+                    { "bSortable": false, "aTargets": [5] }
+                    ]
+                });
+            } else {
+                $('#FilesGrid').append("<span>No Files Found.</span>");
+            }
+        },
+        error: function (data) {
+            $('#FilesGrid').append("<span>Error Retreiving Item. Error : " + JSON.stringify(data) + "</span>");
+        }
+    });
+};
+//Generate html table values
+function GenerateTableFromJson(objArray) {
+    var tableContent =
+        '<table id="FilesTable" class="table table-striped table-bordered" cellspacing="0" width="100%">' +
+            '<thead><tr>' + '<th>ID</th>' + '<th>Title</th>' + '<th>FileName</th>' + '<th>FileType</th>' +
+            '<th>Team</th>' + '<th>Actions</th>' + '</tr></thead>';
+    for (var i = 0; i < objArray.length; i++) {
+        tableContent += '<tr>';
+        tableContent += '<td>' + objArray[i].Id + '</td>';
+        tableContent += '<td>' + objArray[i].Title + '</td>';
+        tableContent += '<td>' + objArray[i].FileName + '</td>';
+        tableContent += '<td>' + objArray[i].FileType + '</td>';
+        tableContent += '<td>' + objArray[i].Team + '</td>';
+        tableContent += "<td><a id='" + objArray[i].Id + "' href='#' style='color: orange' class='confirmEditFileLink'>" +
+            "<i class='glyphicon glyphicon-pencil' title='Edit Item'></i></a>&nbsp&nbsp";
+        tableContent += "<a id='" + objArray[i].Id + "' href='#' style='color: red' class='confirmDeleteFileLink'>" +
+            "<i class='glyphicon glyphicon-remove' title='Delete File'></i></a>&nbsp&nbsp";
+        tableContent += "<a id='" + objArray[i].Id + "' href='#' class='confirmListItemDetailsLink'>" +
+            "<i class='glyphicon glyphicon-cog' title='Link to List Item'></i></a></td>";
+        tableContent += '</tr>';
+    }
+    return tableContent;
+};
+// Edit button click event
+$(document).on('click', '.confirmEditFileLink', function (e) {
+    e.preventDefault();
+    var id = this.id;
+    var requestUri = "../_api/web/lists/getByTitle('Files')/items(" + id + ")";
+    $.ajax({
+        url: requestUri,
+        method: "GET",
+        contentType: "application/json;odata=verbose",
+        headers: { "accept": "application/json;odata=verbose" },
+        success: function (data) {
+            $('#fileTitle').val(data.d.Title);
+            $('#fileName').val(data.d.FileName);
+            $('#fileType').val(data.d.FileType);
+            $('#team').val(data.d.Team);
+            $('#fileId').val(data.d.Id);
+            $('#myModalLabel').html('Edit Item');
+            $('#myModalNorm').modal('show');
+            $("#etag").val(data.d.__metadata.etag);
+        }
+    });
+});
+//Link to files list item
+$(document).on('click', '.confirmListItemDetailsLink', function (e) {
+    e.preventDefault();
+    var id = this.id;
+    var requestUri = "../Lists/Files/DispForm.aspx?ID=" + id;
+    window.location.href = requestUri;
+});
+// Delete button click event
+$(document).on('click', '.confirmDeleteFileLink', function (e) {
+    e.preventDefault();
+    var id = this.id;
+    BootstrapDialog.show({
+        size: BootstrapDialog.SIZE_SMALL,
+        type: BootstrapDialog.TYPE_DANGER,
+        title: "Delete confirmation",
+        message: "Are you sure you want to Delete this Item?",
+        buttons: [
+            {
+                label: "Confirm",
+                cssClass: 'btn-primary',
+                action: function (dialog) {
+                    dialog.close();
+                    var restUrl = "../_api/web/lists/GetByTitle('Files')/items(" + id + ")";
+                    jQuery.ajax({
+                        url: restUrl,
+                        type: "DELETE",
+                        headers: {
+                            Accept: "application/json;odata=verbose",
+                            "X-RequestDigest": $("#__REQUESTDIGEST").val(),
+                            "IF-MATCH": "*"
+                        }
+                    });
+                    toastr.success("Successfully Deleted.", "Success");
+                    PopulateGrid();
+                }
+            },
+            {
+                label: "Cancel",
+                action: function (dialog) {
+                    dialog.close();
+                }
+            }
+        ]
+    });
+});
+//Update Model Label
+function updateFormLabel() {
+    $('#myModalLabel').html('Add New Item');
+};
+//Populate then display model dialog for add file button clicked
+function addNewFile() {
+    $('#myModalLabel').html('Add New Item');
+    $('#fileTitle').val('');
+    $('#fileName').val('');
+    $('#fileType').val('');
+    $('#team').val('');
+    $('#fileId').val('');
+    $('#myModalNorm').modal('show');
+};
+//Edit item function
+function UpdateFiles(id) {
+    var fileTitle = $("#fileTitle").val();
+    var fileName = $("#fileName").val();
+    var fileType = $("#fileType").val();
+    var team = $("#team").val();
+    var eTag = $("#etag").val();
+    var requestUri = "../_api/web/lists/getByTitle('Files')/items(" + id + ")";
+    var requestHeaders = {
+        "accept": "application/json;odata=verbose",
+        "X-HTTP-Method": "MERGE",
+        "X-RequestDigest": $('#__REQUESTDIGEST').val(),
+        "If-Match": eTag
+    }
+    var fileData = {
+        __metadata: { "type": "SP.Data.FilesListItem" },
+        Title: fileTitle,
+        FileName: fileName,
+        FileType: fileType,
+        Team: team
+    };
+    var requestBody = JSON.stringify(fileData);
+
+    return $.ajax({
+        url: requestUri,
+        type: "POST",
+        contentType: "application/json;odata=verbose",
+        headers: requestHeaders,
+        data: requestBody
+    });
+}
+//Add File function
+var addFile = function (fileTitle, fileName, fileType, team) {
+    var requestUri = "../_api/web/lists/getByTitle('Files')/items";
+    var requestHeaders = {
+        "accept": "application/json;odata=verbose",
+        "content-type": "application/json;odata=verbose",
+        "X-RequestDigest": $('#__REQUESTDIGEST').val()
+    }
+    var fileData = {
+        __metadata: { "type": "SP.Data.FilesListItem" },
+        Title: fileTitle,
+        FileName: fileName,
+        FileType: fileType,
+        Team: team
+    };
+    var requestBody = JSON.stringify(fileData);
+    return $.ajax({
+        url: requestUri,
+        type: "POST",
+        headers: requestHeaders,
+        data: requestBody
+    });
+
+};
